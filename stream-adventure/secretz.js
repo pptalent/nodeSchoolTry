8b243635a8e425b7d8d8c6bfe17037287e6a2149
wwayne <wayneup@163.com>
2015-07-27 22:15:56 +0800

    over

--- stream-adventure/secretz.js
+++ stream-adventure/secretz.js
@@ -1,36 +1,44 @@
-var tar=require('tar'),
-    crypto=require('crypto'),
-    zlib=require('zlib'),
-    through=require('through');
+// var tar=require('tar'),
+//     crypto=require('crypto'),
+//     zlib=require('zlib'),
+//     through=require('through');
 
-var stream=crypto.createDecipher(process.argv[2],process.argv[3]);
-var parser=tar.Parse();
-parser.on('entry',function(entry){
+// var stream=crypto.createDecipher(process.argv[2],process.argv[3]);
+// var parser=tar.Parse();
+// parser.on('entry',function(entry){
+//   if(entry.type!=='File') return;
+//   var hasher=crypto.createHash('md5',{encoding:'hex'});
+//     entry.pipe(hasher).pipe(through(null,function(){
+//         this.queue(" "+entry.path+"\n");
+//     })).pipe(process.stdout);
+// });
+// process.stdin
+//   .pipe(stream)
+//   .pipe(zlib.createGunzip())
+//   .pipe(parser)
+//   ;
+
+var through = require("through");
+var crypto = require("crypto");
+var tar = require("tar");
+var zlib = require("zlib");
+
+var parser = tar.Parse();
+parser.on('entry', function(entry) {
   if(entry.type!=='File') return;
   var hasher=crypto.createHash('md5',{encoding:'hex'});
-    entry.pipe(hasher).pipe(through(null,function(){
-        this.queue(" "+entry.path+"\n");
-    })).pipe(process.stdout);
-});
+  entry.pipe(hasher)
+    .pipe(through(function(data) {
+      this.queue(data+" "+entry.path+"\n");
+    },function(){
+      this.queue(null);
+    }))
+    .pipe(process.stdout);
+})
+
+var deCrypto = crypto.createDecipher(process.argv[2], process.argv[3]);
+
 process.stdin
-  .pipe(stream)
+  .pipe(deCrypto)
   .pipe(zlib.createGunzip())
   .pipe(parser)
-  ;
-// var crypto = require('crypto');
-// var decipher = crypto.createDecipher(process.argv[2], process.argv[3]);
-// var gunzip = require('zlib').createGunzip();
-// var parser = require('tar').Parse();
-// var through = require('through');
-//
-// parser.on('entry', function (e) {
-// if (e.type !== 'File') {
-// return ;
-// }
-// var hasher = crypto.createHash('md5', {encoding : 'hex'});
-// e.pipe(hasher).pipe(through(null, function () {
-// this.queue(' ' + e.path + '\n');
-// })).pipe(process.stdout);
-// });
-//
-// process.stdin.pipe(decipher).pipe(gunzip).pipe(parser);
